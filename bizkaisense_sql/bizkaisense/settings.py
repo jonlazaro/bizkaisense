62ebab5285b2e8ae9fc652601ed038ed6c2e3972
Jon Lazaro <lazaro.jon@gmail.com>
2013-07-17 14:41:41 +0200

    New settings

--- bizkaisense_sql/bizkaisense/settings.py
+++ bizkaisense_sql/bizkaisense/settings.py
@@ -17,8 +17,8 @@ LANGUAGES = (
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'dev.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -79,7 +79,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     #'http://helheim.deusto.es/bizkaisense/static',
-    '/home/jon/bizkaisense/static',
+    '/home/jon/morelab/bizkaisense/bizkaisense_sql/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -115,7 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/jon/bizkaisense/templates',
+    '/home/jon/morelab/bizkaisense/bizkaisense_sql/templates',
 )
 
 INSTALLED_APPS = (
