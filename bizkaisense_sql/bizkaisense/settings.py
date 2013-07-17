6f1dd83f96f5324589c651544392aa59669c94e9
Jon Lazaro <lazaro.jon@gmail.com>
2013-07-17 15:10:06 +0200

    Some minor changes in templates and settings

--- bizkaisense_sql/bizkaisense/settings.py
+++ bizkaisense_sql/bizkaisense/settings.py
@@ -1,4 +1,6 @@
 # Django settings for bizkaisense project.
+from database_config import databases
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -15,16 +17,7 @@ LANGUAGES = (
     ('eu', 'Basque'),
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+DATABASES = databases
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -78,8 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #'http://helheim.deusto.es/bizkaisense/static',
-    '/home/jon/morelab/bizkaisense/bizkaisense_sql/static',
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../static')),
 )
 
 # List of finder classes that know how to find static files in
@@ -115,7 +107,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/jon/morelab/bizkaisense/bizkaisense_sql/templates',
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../templates')),
 )
 
 INSTALLED_APPS = (
