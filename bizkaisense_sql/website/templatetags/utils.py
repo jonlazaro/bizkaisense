83fdfce41f147c404bbc5bd7c1aebbe740f79f71
Mikel Emaldi <m.emaldi@gmail.com>
2014-06-09 17:53:17 +0200

    Fixed some bugs

--- bizkaisense_sql/website/templatetags/utils.py
+++ bizkaisense_sql/website/templatetags/utils.py
@@ -1,5 +1,8 @@
 from django import template
 
+register = template.Library()
+
+@register.filter(name='replace')
 def replace(value, arg):
     s = arg.split(':')
     return value.replace(s[0], s[1])
